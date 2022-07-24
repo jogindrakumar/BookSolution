3745e574505e73b36dd98a4f1536cdbd3ab509da
jogindrakumar <joginder9868@outlook.com>
2022-07-24 11:18:34 +0530

    Add project files.

---
+++ BookSolutionWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookSolutionWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
