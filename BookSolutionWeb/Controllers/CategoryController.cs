9606e1d22fc1f86869e06a177579c84128fbe9f4
jogindrakumar <joginder9868@outlook.com>
2022-07-28 13:07:13 +0530

    create-category-controller-and-category-view

---
+++ BookSolutionWeb/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookSolutionWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
