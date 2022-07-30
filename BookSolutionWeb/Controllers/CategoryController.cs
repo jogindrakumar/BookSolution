b8d11cd5aa9adebb7c497b78e545978e9ce31df1
jogindrakumar <joginder9868@outlook.com>
2022-07-30 11:10:07 +0530

    edit-category-update-controller-view

--- BookSolutionWeb/Controllers/CategoryController.cs
+++ BookSolutionWeb/Controllers/CategoryController.cs
@@ -42,5 +42,39 @@ namespace BookSolutionWeb.Controllers
             return View(obj);
 
         }
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            //var categoryFromDbSingle = _db.Categories.SingleOrDefault(c => c.Id == id); 
+            //var categoryFromDbFirst = _db.Categories.FirstOrDefault(c => c.Id == id);
+            if (categoryFromDb == null) { 
+                return  NotFound(); 
+            }
+
+            return View(categoryFromDb);
+        }
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder Cannot exactly match the Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+        }
     }
 }
